3b302f54899337a9570d0723fe463caae7ef2726
Manuel <69732401+manu-cadena@users.noreply.github.com>
2026-06-01 11:08:32 +0200

    feat: initialize foundry project with ERC-4337 minimal account
    Set up Foundry project structure for account abstraction development.
    Add MinimalAccount contract implementing IAccount interface with
    validateUserOp skeleton. Install eth-infinitism account-abstraction
    and forge-std dependencies.

---
+++ README.md
@@ -0,0 +1,5 @@
+# About
+
+1. Create a basic AA on Ethereum
+1. Create a basic AA on zkSync
+1. Deploy, and send a userOp/ transaction through them
---
+++ src/ethereum/MinimalAccount.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.30;
+
+import {IAccount} from "lib/account-abstraction/contracts/interfaces/IAccount.sol";
+import {PackedUserOperation} from "lib/account-abstraction/contracts/interfaces/PackedUserOperation.sol";
+
+contract MinimalAccount {
+    function validateUserOp(PackedUserOperation calldata userOp, bytes32 userOpHash, uint256 missingAccountFounds)
+        external
+        returns (uint256 validationData)
+    {}
+}
