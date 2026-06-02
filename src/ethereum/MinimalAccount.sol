00c5699a73fdd5221e946154488fcd20e9d27fd1
Manuel <69732401+manu-cadena@users.noreply.github.com>
2026-06-02 12:30:01 +0200

    feat: add deploy scripts, helper config, and fix IEntryPoint import
    Add DeployMinimal, HelperConfig, and SendPackedUserOp scripts.
    Install foundry-devops dependency. Fix IEntryPoint import casing
    and update OpenZeppelin remapping path. Use flexible pragma version.

--- src/ethereum/MinimalAccount.sol
+++ src/ethereum/MinimalAccount.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.30;
+pragma solidity ^0.8.30;
 
 import {IAccount} from "lib/account-abstraction/contracts/interfaces/IAccount.sol";
 import {PackedUserOperation} from "lib/account-abstraction/contracts/interfaces/PackedUserOperation.sol";
@@ -7,7 +7,7 @@ import {Ownable} from "lib/openzeppelin-contracts/contracts/access/Ownable.sol";
 import {MessageHashUtils} from "lib/openzeppelin-contracts/contracts/utils/cryptography/MessageHashUtils.sol";
 import {ECDSA} from "lib/openzeppelin-contracts/contracts/utils/cryptography/ECDSA.sol";
 import {SIG_VALIDATION_FAILED, SIG_VALIDATION_SUCCESS} from "lib/account-abstraction/contracts/core/Helpers.sol";
-import {IentryPoint} from "lib/account-abstraction/contracts/interfaces/IEntryPoint.sol";
+import {IEntryPoint} from "lib/account-abstraction/contracts/interfaces/IEntryPoint.sol";
 
 contract MinimalAccount is IAccount, Ownable {
     /*////////////////////////////////////////////////////////////////////////
@@ -21,7 +21,7 @@ contract MinimalAccount is IAccount, Ownable {
                                 STATE VARIABLES
     /////////////////////////////////////////////////////////////////////////*/
 
-    IentryPoint private immutable i_entryPoint;
+    IEntryPoint private immutable i_entryPoint;
 
     /*////////////////////////////////////////////////////////////////////////
                                 MODIFIERS
@@ -46,7 +46,7 @@ contract MinimalAccount is IAccount, Ownable {
     /////////////////////////////////////////////////////////////////////////*/
 
     constructor(address entryPoint) Ownable(msg.sender) {
-        i_entryPoint = IentryPoint(entryPoint);
+        i_entryPoint = IEntryPoint(entryPoint);
     }
 
     receive() external payable {}
