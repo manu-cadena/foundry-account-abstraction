bee9d8807a0e29ccc894a90cb040c8e1d0501c88
Manuel <69732401+manu-cadena@users.noreply.github.com>
2026-06-02 12:52:58 +0200

    feat: add test suite and fix signature validation logic
    Add MinimalAccountTest with tests for owner execution, access control,
    signature recovery, userOp validation, and EntryPoint integration.
    Fix critical bug in _validateSignature where signer check was inverted.
    Remove excessive gas forwarding in _payPrefund to prevent EntryPoint
    reentrancy errors. Update execute modifier to allow owner calls.
    Add Makefile and update foundry.toml with optimizer and fs permissions.
    Refactor imports to use @openzeppelin remapping.

--- src/ethereum/MinimalAccount.sol
+++ src/ethereum/MinimalAccount.sol
@@ -3,68 +3,66 @@ pragma solidity ^0.8.30;
 
 import {IAccount} from "lib/account-abstraction/contracts/interfaces/IAccount.sol";
 import {PackedUserOperation} from "lib/account-abstraction/contracts/interfaces/PackedUserOperation.sol";
-import {Ownable} from "lib/openzeppelin-contracts/contracts/access/Ownable.sol";
-import {MessageHashUtils} from "lib/openzeppelin-contracts/contracts/utils/cryptography/MessageHashUtils.sol";
-import {ECDSA} from "lib/openzeppelin-contracts/contracts/utils/cryptography/ECDSA.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {MessageHashUtils} from "@openzeppelin/contracts/utils/cryptography/MessageHashUtils.sol";
+import {ECDSA} from "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
 import {SIG_VALIDATION_FAILED, SIG_VALIDATION_SUCCESS} from "lib/account-abstraction/contracts/core/Helpers.sol";
 import {IEntryPoint} from "lib/account-abstraction/contracts/interfaces/IEntryPoint.sol";
 
 contract MinimalAccount is IAccount, Ownable {
-    /*////////////////////////////////////////////////////////////////////////
-                                ERRORS
-    /////////////////////////////////////////////////////////////////////////*/
+    /*//////////////////////////////////////////////////////////////
+                                 ERRORS
+    //////////////////////////////////////////////////////////////*/
     error MinimalAccount__NotFromEntryPoint();
     error MinimalAccount__NotFromEntryPointOrOwner();
     error MinimalAccount__CallFailed(bytes);
+    error MinimalAccount__PayPreFundFailed();
 
-    /*////////////////////////////////////////////////////////////////////////
-                                STATE VARIABLES
-    /////////////////////////////////////////////////////////////////////////*/
-
+    /*//////////////////////////////////////////////////////////////
+                            STATE VARIABLES
+    //////////////////////////////////////////////////////////////*/
     IEntryPoint private immutable i_entryPoint;
 
-    /*////////////////////////////////////////////////////////////////////////
-                                MODIFIERS
-    /////////////////////////////////////////////////////////////////////////*/
-
-    modifier requireFromEntrypoint() {
+    /*//////////////////////////////////////////////////////////////
+                               MODIFIERS
+    //////////////////////////////////////////////////////////////*/
+    modifier requireFromEntryPoint() {
         if (msg.sender != address(i_entryPoint)) {
             revert MinimalAccount__NotFromEntryPoint();
         }
         _;
     }
 
-    modifier requireFromEntrypointOrOwner() {
+    modifier requireFromEntryPointOrOwner() {
         if (msg.sender != address(i_entryPoint) && msg.sender != owner()) {
             revert MinimalAccount__NotFromEntryPointOrOwner();
         }
         _;
     }
 
-    /*////////////////////////////////////////////////////////////////////////
-                                FUNCTIONS
-    /////////////////////////////////////////////////////////////////////////*/
-
+    /*//////////////////////////////////////////////////////////////
+                               FUNCTIONS
+    //////////////////////////////////////////////////////////////*/
     constructor(address entryPoint) Ownable(msg.sender) {
         i_entryPoint = IEntryPoint(entryPoint);
     }
 
     receive() external payable {}
 
-    /*////////////////////////////////////////////////////////////////////////
-                        EXTERNAL FUNCTIONS
-    /////////////////////////////////////////////////////////////////////////*/
-    function execute(address dest, uint256 value, bytes calldata functionData) external requireFromEntrypoint {
+    /*//////////////////////////////////////////////////////////////
+                           EXTERNAL FUNCTIONS
+    //////////////////////////////////////////////////////////////*/
+    function execute(address dest, uint256 value, bytes calldata functionData) external requireFromEntryPointOrOwner {
         (bool success, bytes memory result) = dest.call{value: value}(functionData);
         if (!success) {
             revert MinimalAccount__CallFailed(result);
         }
     }
 
-    // A signature is valid, if it's MinimalAccounts owner
+    // A signature is valid, if it's the MinimalAccount owner
     function validateUserOp(PackedUserOperation calldata userOp, bytes32 userOpHash, uint256 missingAccountFunds)
         external
-        requireFromEntrypoint
+        requireFromEntryPoint
         returns (uint256 validationData)
     {
         validationData = _validateSignature(userOp, userOpHash);
@@ -72,9 +70,9 @@ contract MinimalAccount is IAccount, Ownable {
         _payPrefund(missingAccountFunds);
     }
 
-    /*////////////////////////////////////////////////////////////////////////
-                        INTERNAL FUNCTIONS
-    ///////////////////////////////////////////////////////////////////////*/
+    /*//////////////////////////////////////////////////////////////
+                           INTERNAL FUNCTIONS
+    //////////////////////////////////////////////////////////////*/
     // EIP-191 version of the signed hash
     function _validateSignature(PackedUserOperation calldata userOp, bytes32 userOpHash)
         internal
@@ -83,7 +81,7 @@ contract MinimalAccount is IAccount, Ownable {
     {
         bytes32 ethSignedMessageHash = MessageHashUtils.toEthSignedMessageHash(userOpHash);
         address signer = ECDSA.recover(ethSignedMessageHash, userOp.signature);
-        if (signer == owner()) {
+        if (signer != owner()) {
             return SIG_VALIDATION_FAILED;
         }
         return SIG_VALIDATION_SUCCESS;
@@ -91,17 +89,17 @@ contract MinimalAccount is IAccount, Ownable {
 
     function _payPrefund(uint256 missingAccountFunds) internal {
         if (missingAccountFunds != 0) {
-            (bool success,) = payable(msg.sender).call{value: missingAccountFunds, gas: type(uint256).max}("");
-            require(success, "Failed to pay prefund");
+            (bool success,) = payable(msg.sender).call{value: missingAccountFunds}("");
+            if (!success) {
+                revert MinimalAccount__PayPreFundFailed();
+            }
         }
     }
 
-    /*////////////////////////////////////////////////////////////////////////
-                                    GETTERS
-     ///////////////////////////////////////////////////////////////////////*/
-
+    /*//////////////////////////////////////////////////////////////
+                                GETTERS
+    //////////////////////////////////////////////////////////////*/
     function getEntryPoint() external view returns (address) {
         return address(i_entryPoint);
     }
 }
-
