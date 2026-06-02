64a4fced8f74555d79137ffec20fe677e22f73f4
Manuel <69732401+manu-cadena@users.noreply.github.com>
2026-06-02 11:40:07 +0200

    feat: add EntryPoint integration and access control
    Store immutable EntryPoint reference in constructor and restrict
    validateUserOp to only be callable by the EntryPoint via
    requireFromEntrypoint modifier. Add getEntryPoint getter.

--- src/ethereum/MinimalAccount.sol
+++ src/ethereum/MinimalAccount.sol
@@ -7,13 +7,28 @@ import {Ownable} from "lib/openzeppelin-contracts/contracts/access/Ownable.sol";
 import {MessageHashUtils} from "lib/openzeppelin-contracts/contracts/utils/cryptography/MessageHashUtils.sol";
 import {ECDSA} from "lib/openzeppelin-contracts/contracts/utils/cryptography/ECDSA.sol";
 import {SIG_VALIDATION_FAILED, SIG_VALIDATION_SUCCESS} from "lib/account-abstraction/contracts/core/Helpers.sol";
+import {IentryPoint} from "lib/account-abstraction/contracts/interfaces/IEntryPoint.sol";
 
 contract MinimalAccount is IAccount, Ownable {
-    constructor() Ownable(msg.sender) {}
+    error MinimalAccount__NotFromEntryPoint();
+
+    IentryPoint private immutable i_entryPoint;
+
+    modifier requireFromEntrypoint() {
+        if (msg.sender != address(i_entryPoint)) {
+            revert MinimalAccount__NotFromEntryPoint();
+        }
+        _;
+    }
+
+    constructor(address entryPoint) Ownable(msg.sender) {
+        i_entryPoint = IentryPoint(entryPoint);
+    }
 
     // A signature is valid, if it's MinimalAccounts owner
     function validateUserOp(PackedUserOperation calldata userOp, bytes32 userOpHash, uint256 missingAccountFunds)
         external
+        requireFromEntrypoint
         returns (uint256 validationData)
     {
         validationData = _validateSignature(userOp, userOpHash);
@@ -40,6 +55,15 @@ contract MinimalAccount is IAccount, Ownable {
             (bool success,) = payable(msg.sender).call{value: missingAccountFunds, gas: type(uint256).max}("");
             require(success, "Failed to pay prefund");
         }
+    }
+
+    /*////////////////////////////////////////////////////////////////////////
+                                    GETTERS
+     */
+    /////////////////////////////////////////////////////////////////////////
+
+    function getEntryPoint() external view returns (address) {
+        return address(i_entryPoint);
     }
 }
 
